9fc1b1498a2c397e92743da91fcfa254ef25f443
sas1711019 <104480828+sas1711019@users.noreply.github.com>
2022-06-06 12:19:20 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+})
+basic.forever(function () {
+    music.playTone(880, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(988, music.beat(BeatFraction.Half))
 })
