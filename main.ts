959394eed66462525cc17584487db3ba167ab28c
sas1711019 <104480828+sas1711019@users.noreply.github.com>
2022-06-09 11:38:35 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,31 @@
-input.onButtonPressed(Button.A, function () {
-    music.playTone(659, music.beat(BeatFraction.Whole))
-    music.playTone(622, music.beat(BeatFraction.Whole))
-    music.playTone(659, music.beat(BeatFraction.Whole))
-    music.playTone(622, music.beat(BeatFraction.Whole))
-    music.playTone(659, music.beat(BeatFraction.Whole))
-    music.playTone(494, music.beat(BeatFraction.Whole))
-    music.playTone(587, music.beat(BeatFraction.Whole))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    basic.pause(100)
-    music.playTone(165, music.beat(BeatFraction.Whole))
-    music.playTone(220, music.beat(BeatFraction.Whole))
-    music.playTone(247, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Square)
 })
-music.playTone(494, music.beat(BeatFraction.Whole))
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )))
-    basic.showNumber(sonar.ping(
+    if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ))
+    ) > 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # #
+            . # . . #
+            . # # # #
+            `)
+    }
 })
